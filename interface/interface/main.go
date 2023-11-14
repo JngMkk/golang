f83d894d12333c5f52a9e0f590f891be8af6a896
JngMkk <kjm0817ss@gmail.com>
2023-11-14 17:20:59 +0900

    add: interface

---
+++ interface/interface/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/JngMkk/golang/interface/interface/interfaces"
+)
+
+func main() {
+	in := bytes.NewReader([]byte("example"))
+	out := &bytes.Buffer{}
+	fmt.Print("Stdout on Copy = ")
+	if err := interfaces.Copy(in, out); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("out bytes buffer =", out.String())
+	fmt.Print("Stdout on PipeExample = ")
+	if err := interfaces.PipeExample(); err != nil {
+		panic(err)
+	}
+}
---
+++ interface/interface4/main.go
@@ -0,0 +1,39 @@
+// 서로 다른 인터페이스를 맞춰주기 위해 어댑터 패턴 사용 예시
+package main
+
+import "fmt"
+
+type Database interface {
+	Get()
+	Set()
+}
+
+type CDatabase struct {
+}
+
+func (c CDatabase) GetData() {
+
+}
+
+func (c CDatabase) SetData() {
+
+}
+
+// 어댑터
+type Wrapper struct {
+	cdb CDatabase
+}
+
+func (w Wrapper) Get() {
+	w.cdb.GetData()
+}
+
+func (w Wrapper) Set() {
+	w.cdb.SetData()
+}
+
+func main() {
+	var cdatabase CDatabase
+	database := Wrapper{cdatabase}
+	fmt.Println(database)
+}
